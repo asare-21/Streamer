e0299edccfa812dcf7cbaaec69ab79e5ba505d0c
Joseph <jasare4525@users.noreply.github.com>
2020-06-29 15:09:07 +0000

    second commit

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,31 @@
+import {slide} from './slide.js'
+import {Return} from './slide.js'
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+let count = true
 document.querySelector('.burgerContainer').addEventListener('click',(e)=> {
-   if(e.target.classList.contains('line')){
+
+   if(e.target.classList.contains('line') || e.target.classList.contains('burgerContainer') ||e.target.classList.contains('fas')){
        document.querySelector('.side-nav').classList.toggle('toggle')
+       document.querySelectorAll('.line').forEach(line =>{
+           line.classList.toggle('animate')
+       })
+       document.querySelectorAll('p').forEach(p => {
+           p.classList.toggle('show')
+       })
+       const l = document.querySelector('.burgerContainer')
+       if(count){
+           l.innerHTML = `<i class="fas fa-arrow-right"></i>`
+           count = false
+       }
+       else{
+           l.innerHTML = ` <div class="line"></div>
+           <div class="line"></div>
+           <div class="line"></div>`
+           count = true
+       }
    }
 }
 )
+right.addEventListener('click',slide)
+left.addEventListener('click',Return)
---
+++ modules/slide.js
@@ -0,0 +1,33 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const num = 10
+let incr = 0
+const length = document.querySelector('.slide').childElementCount
+const s = (100/length)
+ 
+export function slide(){
+ incr = incr + s
+  if(incr !== length*10){
+    document.querySelector('.slide').style = `transform:translateX(${incr}%)`
+  }
+  if(incr == length*10){
+    document.querySelector('.slide').style = `transform:translateX(0%)`
+    document.querySelector('.slide').style = `transition:all 2s linear`
+    incr = 0
+      }
+}
+console.log(incr)
+export function Return(){
+ incr = incr - s
+  if(incr !== length*10){
+    document.querySelector('.slide').style = `transform:translateX(${incr}%)`
+  }
+
+  if(incr < 0 ){
+    document.querySelector('.slide').style = `transform:translateX(0%)`
+    document.querySelector('.slide').style = `transition:all 2s linear`
+    incr = 0
+      }
+      console.log(incr)
+
+}
