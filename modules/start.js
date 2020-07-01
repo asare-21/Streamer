5107a96910fb2325bfca9a79521eef27d96e6f75
Joseph <52238457+jasare4525@users.noreply.github.com>
2020-07-01 02:55:15 +0000

    Update start.js

--- modules/start.js
+++ modules/start.js
@@ -1,2 +1,6 @@
 // console.log(process.env.S )
 require('dotenv').config()
+const fs = require('fs')
+fs.writeFileSync('./.env', `SpaceID=${process.env.SpaceID},
+
+ContentDeliveryApi=${process.env.ContentDeliveryApi}`)
