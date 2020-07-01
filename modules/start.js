1a538269114d3a5ef7ebec62eebf9943129957ea
Joseph <jasare4525@users.noreply.github.com>
2020-07-01 09:07:06 +0000

    pub

--- modules/start.js
+++
@@ -1,6 +0,0 @@
-// console.log(process.env.S )
-require('dotenv').config()
-const fs = require('fs')
-fs.writeFileSync('./.env', `SpaceID=${process.env.SpaceID},
-
-ContentDeliveryApi=${process.env.ContentDeliveryApi}`)
